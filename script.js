9c6d7a37b704fad102e970f829836e5ec2a24cfd
BobbyHuart1 <bobby.huart@sky.uk>
2022-10-27 15:10:04 +0100

    fixed button and styling

--- script.js
+++ script.js
@@ -1,43 +1,65 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'afdcdda00amsh2752c90f576065fp19e940jsn2cf71a54bb53',
+		'X-RapidAPI-Key': '3349e34336mshbb3e5a86990215fp1e2e62jsn598d9dbc447e',
 		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
 	}
 };
 
+function createPosters(title,description,service,posterURL){
+    let poster = document.createElement('div')
+
+    let titleTag = document.createElement('h2')
+    titleTag.innerHTML = title
+
+    let imageTag = document.createElement('img') //creating an image element to add the poster image to
+    imageTag.src = posterURL
+
+    poster.append(titleTag,imageTag)
+}
+
 async function getMovie(movieName, country, service){
-    const response = await fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=${countriesAbv[country]}&service=${service}&type=movie&keyword=${movieName}&page=1&output_language=en&language=en`, options)
+    //const response = await fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=${countriesAbv[country]}&service=${service}&type=movie&keyword=${movieName}&page=1&output_language=en&language=en`, options)
+    const response = await fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=${service}&type=movie&keyword=${movieName}&page=1&output_language=en&language=en`, options)
     const data = await response.json()
-    console.log(data)
-    let keys = Object.keys(data.results)
+    let keys = Object.keys(data.results) //loop through the returned data.results object's keys, returned data is weirdly formatted
     for (let key of keys){
-		console.log(data.results[key].title)
-		console.log(data.results[key].overview)
+		let title = data.results[key].title  //each movie is stored as on object with its key as an integer e.g data.results[0] = {title: 'finding nemo', country: [us,uk] ....}
+		let description = data.results[key].overview
+        let posterURL = data.results[key].posterURLs['original']
+        createCards(title,description,service,posterURL)
+
 	}
 }
 
 document.getElementById('search-button').addEventListener('click', () => {
+    //getting search filters from the form to pass to the fetch request
     let movieName = document.getElementById('movie-name').value
-    let country = document.getElementById("country-dropdown").value
     let service = document.getElementById('service-dropdown').value
-    console.log(country)
-    getMovie(movieName,country,service)
+    getMovie(movieName,currentCountry,service)
 })
 
+// funciton to add countries to dropdown menu
+currentCountry = 'United Kingdom' //set default as uk
 function addCountries(countriesAbv){
     const dropdown = document.getElementById('country-dropdown')
     let keys = Object.keys(countriesAbv)
-    for (let key of keys){
-        let newCountry = document.createElement('option')
-        newCountry.value = key
-        newCountry.innerHTML = key
-        if (key === 'United Kingdom'){
-            newCountry.selected = true
-        }
-        dropdown.append(newCountry)
-        
+    for (let key of keys){ //loop through country object keys: object keys = country name, key value = country abbreviation
+        let listItem = document.createElement('li')
+        let countryButton = document.createElement('button') //create dropdown menu item element
+        countryButton.innerHTML = key //set elements text to country name
+
+        countryButton.classList.add('country-button')
+        countryButton.type = 'button'
+
+        countryButton.addEventListener('click', () => {
+            document.getElementById('dropdown-country-button').innerHTML = `Country: ${key}`
+        })
+
+        listItem.append(countryButton)
+        dropdown.append(listItem) //add element to dropdown menu
     }
+
 }
 
 function addServices(services){
@@ -86,3 +108,4 @@ addCountries(countriesAbv)
 addServices(services)
 
 // Displaying movie info searching for user
+

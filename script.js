a97c9fd47b86625a15d74087ee5def50c7690d3e
BobbyHuart1 <bobby.huart@sky.uk>
2022-10-28 11:45:27 +0100

    mvp done, description needed

--- script.js
+++ script.js
@@ -1,30 +1,68 @@
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '3349e34336mshbb3e5a86990215fp1e2e62jsn598d9dbc447e',
+		'X-RapidAPI-Key': '591950c54bmsh971da8756a9c91bp155940jsn7aa65193a364',
 		'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
 	}
 };
 
 function createPosters(title,description,service,posterURL){
+    //console.log(title)
     let poster = document.createElement('div')
     poster.classList.add('newPoster')
 
     let titleTag = document.createElement('h2')
     titleTag.innerHTML = title
+    titleTag.classList.add('poster-title')
 
     let imageTag = document.createElement('img') //creating an image element to add the poster image to
     imageTag.src = posterURL
+    imageTag.classList.add('poster-image') 
 
-    poster.append(titleTag,imageTag)
+    
+
+    let countryTag = document.createElement('p')
+    countryTag.innerHTML = currentCountry
+    countryTag.classList.add('poster-country')
+
+    let serviceTag = document.createElement('p')
+    serviceTag.innerHTML = currentService
+    serviceTag.classList.add('poster-service')
+
+
+    poster.append(titleTag,imageTag,countryTag,serviceTag)
     document.getElementById('poster-section').append(poster)
 }
 
-async function getMovie(movieName, country, service){
-    console.log(service)
-    //const response = await fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=${countriesAbv[country]}&service=${service}&type=movie&keyword=${movieName}&page=1&output_language=en&language=en`, options)
-    const response = await fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=us&service=disney&type=movie&keyword=nemo&page=1&output_language=en&language=en`, options)
+async function getMovie(movieName){
+    //console.log(currentCountry)
+    //console.log(currentService)
+    //console.log(movieName)
+    const response = await fetch(`https://streaming-availability.p.rapidapi.com/search/basic?country=${countriesAbv[currentCountry]}&service=${currentService}&type=movie&keyword=${movieName}&page=1&output_language=en&language=en`, options)
     const data = await response.json()
+
+    
+    let section = document.querySelector(".cards")
+    section.innerHTML = "" // clearing card section for new search or error
+
+    if (data.results.length === 0) {
+        const errorMessage = document.createElement('h2')
+        const sadImage = document.createElement("img")
+        const sadImageCat = document.createElement("img")
+        errorMessage.innerHTML = "Sorry guys, that movie doesn't exist :("; 
+        errorMessage.classList.add('error-message')
+        sadImage.setAttribute("src", "https://media.tenor.com/6CujUsC1CIkAAAAd/crying-black-guy-meme50fps-interpolated-interpolated.gif")
+        sadImage.classList.add("sad-image")
+
+        sadImageCat.setAttribute("src", "https://media.tenor.com/qRqm8fsbFjoAAAAd/cry-cat.gif")
+        sadImageCat.classList.add("sad-image-cat")
+
+        
+        section.append(errorMessage, sadImage, sadImageCat)
+          
+    }
+
+    //console.log(data)
     let keys = Object.keys(data.results) //loop through the returned data.results object's keys, returned data is weirdly formatted
     document.getElementById('popular-releases').style.display = 'none';
     document.getElementById('posterTitle').style.display = 'none';
@@ -32,16 +70,14 @@ async function getMovie(movieName, country, service){
 		let title = data.results[key].title  //each movie is stored as on object with its key as an integer e.g data.results[0] = {title: 'finding nemo', country: [us,uk] ....}
 		let description = data.results[key].overview
         let posterURL = data.results[key].posterURLs['original']
-        createPosters(title,description,service,posterURL)
-
+        createPosters(title,description,currentService,posterURL)
 	}
 }
 
 document.getElementById('search-button').addEventListener('click', () => {
     //getting search filters from the form to pass to the fetch request
     let movieName = document.getElementById('movie-name').value
-    let service = document.getElementById('service-dropdown').value
-    getMovie(movieName,currentCountry,service)
+    getMovie(movieName)
 })
 
 // funciton to add countries to dropdown menu
@@ -50,16 +86,16 @@ function addCountries(countriesAbv){
     const dropdown = document.getElementById('country-dropdown')
     let keys = Object.keys(countriesAbv)
     for (let key of keys){ //loop through country object keys: object keys = country name, key value = country abbreviation
-        let listItem = document.createElement('li')
-        let countryButton = document.createElement('button') //create dropdown menu item element
-        countryButton.innerHTML = key //set elements text to country name
+        let listItem = document.createElement('li') //adding a list item which will contain a button which can be clicked to select service
+        let countryButton = document.createElement('button') 
+        countryButton.innerHTML = key //set button text to country name
 
         countryButton.classList.add('country-button')
-        countryButton.type = 'button'
+        countryButton.type = 'button' //stops button from reloading page when clicked
 
-        countryButton.addEventListener('click', () => {
+        countryButton.addEventListener('click', () => { //adding event listener to the button so country can be changed
             currentCountry = key
-            document.getElementById('dropdown-country-button').innerHTML = `Country: ${key}`
+            document.getElementById('dropdown-country-button').innerHTML = `Country: ${key}` //change dropdown menu text to show what is selected
         })
 
         listItem.append(countryButton)
@@ -68,20 +104,21 @@ function addCountries(countriesAbv){
 
 }
 
-currentService = 'netflix'
-function addServices(services){
+
+currentService = 'disney' //sets the default service if none is selected with the button
+function addServices(services){ //adds all the available services to the dropdown
     const dropdown = document.getElementById('service-dropdown')
     for (let service of services){
-        let listItem = document.createElement('li')
+        let listItem = document.createElement('li') //adding a list item which will contain a button which can be clicked to select country
         let serviceButton = document.createElement('button')
         serviceButton.innerHTML = service.charAt(0).toUpperCase() + service.slice(1)
 
         serviceButton.classList.add('country-button')
-        serviceButton.type = 'button'
+        serviceButton.type = 'button' //stops button from reloading page when clicked
 
-        serviceButton.addEventListener('click', () => {
+        serviceButton.addEventListener('click', () => { //adding event listener to the button so service can be changed
             currentService = service
-            document.getElementById('dropdown-service-button').innerHTML = `Service: ${service}`
+            document.getElementById('dropdown-service-button').innerHTML = `Service: ${service}` //change dropdown menu text to show what is selected
         })
 
         listItem.append(serviceButton)
@@ -121,6 +158,5 @@ countriesAbv = {
 addCountries(countriesAbv)
 addServices(services)
 
-getMovie('nemo','us','netflix')
 // Displaying movie info searching for user
 
